eacc342d8715f6f8c237094b8b4268e5ef6b0369
Qingzhi Luo <tomrony97@gmail.com>
2024-07-08 00:13:50 +1000

    update the original data

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 pip install django
 pip install mysqlclient #(brew install mysql pkg-config)
 pip install django-cors-headers
+
+
+
+ cut -d',' -f3,5,8,9,19 original.csv > pp_data.csv
+ 
+
+
+python manage.py load_data
--- dashboard/management/commands/load_data.py
+++ dashboard/management/commands/load_data.py
@@ -1,6 +1,7 @@
 import csv
 from django.core.management.base import BaseCommand
 from dashboard.models import Pangolin, Location, Division, GISAID
+from datetime import datetime
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
@@ -9,16 +10,36 @@ class Command(BaseCommand):
         with open(csv_file_path, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
+                if row['date'] == 'date':
+                    continue
                 division, _ = Division.objects.get_or_create(division=row['division'])
                 location, _ = Location.objects.get_or_create(location=row['location'])
                 pangolin, _ = Pangolin.objects.get_or_create(pangolin=row['pangolin_lineage'])
 
+                date_str = row['date']
+                if len(date_str) == 4:
+                    year = int(date_str)
+                    if year == 2019:
+                        date_str = f"{date_str}-12-31"
+                    elif year == 2024:
+                        date_str = f"{date_str}-01-31"
+                    else:
+                        date_str = f"{date_str}-06-01"
+                elif len(date_str) == 7:
+                        date_str = f"{date_str}-01"
+
+                try:
+                    date = datetime.strptime(date_str, '%Y-%m-%d').date()
+                except ValueError:
+                    self.stderr.write(self.style.ERROR(f"Invalid date format: {row['date']}"))
+                    continue
+
                 GISAID.objects.create(
-                    ei=row['gisaid_epi_isl'],
-                    division=division,
-                    location=location,
-                    pangolin=pangolin,
-                    date=row['date_submitted']
-                )
+                        ei=row['gisaid_epi_isl'],
+                        division=division,
+                        location=location,
+                        pangolin=pangolin,
+                        date=date
+                    )
 
         self.stdout.write(self.style.SUCCESS('Data loaded successfully'))
